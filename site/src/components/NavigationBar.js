2c73df8dfc877f4316b1df474178a0a364b69d88
dsslleagion <dionisio.leao@fatec.sp.gov.br>
2024-02-18 13:45:57 -0300

    fix_autenticacao_navbar

--- site/src/components/NavigationBar.js
+++ site/src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 import logo from '../assets/logoccv.png';
 import profilePic from '../assets/perfil-sem-foto.png'; // Importe uma imagem de perfil fictícia
@@ -8,9 +8,14 @@ import './NavigationBar.css';
 const NavigationBar = () => {
   const { userData, logout } = useAuth();
   const [dropdownOpen, setDropdownOpen] = useState(false);
+  const location = useLocation();
 
   // Verificar se o usuário está autenticado de forma segura
   const isAuthenticated = userData && userData.token;
+  // Verificar se o usuário é admin
+  const isAdmin = isAuthenticated && userData.cliente && userData.cliente.profile === 'admin';
+  // Verificar se o usuário é do tipo "user"
+  const isUser = isAuthenticated && userData.cliente && userData.cliente.profile === 'user';
 
   const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
@@ -21,6 +26,8 @@ const NavigationBar = () => {
     categoryElement.classList.toggle('active');
   };
 
+  const isCadastroPage = location.pathname === '/cadastro';
+
   return (
     <nav className="nav-container">
       <div className="nav-left">
@@ -30,7 +37,7 @@ const NavigationBar = () => {
         <span className="logo-text">Coletivo Camara Vermelha</span>
       </div>
       <div className="nav-right">
-        {!isAuthenticated && (
+        {!isAuthenticated && !isCadastroPage && (
           <>
             <Link to="/cadastro" className="nav-button">
               Criar Cadastro
@@ -40,17 +47,26 @@ const NavigationBar = () => {
             </Link>
           </>
         )}
+        {!isAuthenticated && isCadastroPage && (
+          <>
+            <Link to="/login" className="nav-button">
+              Fazer Login
+            </Link>
+          </>
+        )}
         {isAuthenticated && (
           <>
-            <div className="nav-category" onClick={() => activateDropdown('dropdown-cadastro')}>
-              <span className="nav-category-title">Cadastros</span>
-              <div className="dropdown-content dropdown-cadastro">
-                <Link to="/cadastroNoticia" className="nav-link"> Notícias</Link>
-                <Link to="/cadastroJornal" className="nav-link"> Jornal</Link>
-                <Link to="/cadastroAdm" className="nav-link"> Usuário</Link>
-                <Link to="/cadastroCandidato" className="nav-link"> Candidato</Link>
+            {isAdmin && (
+              <div className="nav-category" onClick={() => activateDropdown('dropdown-cadastro')}>
+                <span className="nav-category-title">Cadastros</span>
+                <div className="dropdown-content dropdown-cadastro">
+                  <Link to="/cadastroNoticia" className="nav-link"> Notícias</Link>
+                  <Link to="/cadastroJornal" className="nav-link"> Jornal</Link>
+                  <Link to="/cadastroAdm" className="nav-link"> Usuário</Link>
+                  <Link to="/cadastroCandidato" className="nav-link"> Candidato</Link>
+                </div>
               </div>
-            </div>
+            )}
             <div className="nav-category" onClick={() => activateDropdown('dropdown-jornal')}>
               <span className="nav-category-title">Jornal</span>
               <div className="dropdown-content dropdown-jornal">
